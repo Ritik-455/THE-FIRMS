347ba3ced24aa6fb46cd895cbf2395fd5406edac
INDERJEET BHADU <you@example.com>
2024-01-31 11:35:33 +0530

    no-change

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,22 @@ function openNav() {
     document.getElementById("navbar").classList.toggle("start-0");
     document.body.classList.toggle("overflow_hidden");
     document.querySelector(".menu").classList.toggle("cross")
+}
+let mybutton = document.getElementById("top-button")
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block"
+  } else {
+    mybutton.style.display = "none"
   }
+};
+
+function topfuncition() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
 
   $('.slick').slick({
     dots: false,
